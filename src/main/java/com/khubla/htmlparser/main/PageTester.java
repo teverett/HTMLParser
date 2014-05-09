3c05e163232d7cbb8f782bba067e0b2126e9873a
Tom Everett <tom@khubla.com>
2014-05-08 19:58:27 -0600

    cleanup

--- src/main/java/com/khubla/htmlparser/main/PageTester.java
+++ src/main/java/com/khubla/htmlparser/main/PageTester.java
@@ -8,43 +8,33 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import com.khubla.htmlparser.HTMLDocumentParser;
-import com.khubla.htmlparser.grammar.HTMLParser.HtmlDocumentContext;
 
 /**
- * 
  * @author tom
- * 
  */
 public class PageTester {
-	/**
-	 * 
-	 * main
-	 * 
-	 * @throws Exception
-	 * 
-	 */
-	static public void main(String args[]) throws Exception {
-		/*
-		 * the url
-		 */
-		String url = args[0];
-		/*
+   /**
+    * main
+    * 
+    * @throws Exception
+    */
+   static public void main(String args[]) throws Exception {
+      /*
+       * the url
+       */
+      final String url = args[0];
+      /*
 		 * 
 		 */
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		HttpGet httpGet = new HttpGet(url);
-		CloseableHttpResponse response = httpclient.execute(httpGet);
-		InputStream inputStream = null;
-		try {
-			inputStream = response.getEntity().getContent();
-			/*
-			 * parse
-			 */
-			HtmlDocumentContext htmlDocumentContext = HTMLDocumentParser
-					.parse(inputStream);
-		} finally {
-			response.close();
-		}
-
-	}
+      final CloseableHttpClient httpclient = HttpClients.createDefault();
+      final HttpGet httpGet = new HttpGet(url);
+      final CloseableHttpResponse response = httpclient.execute(httpGet);
+      InputStream inputStream = null;
+      try {
+         inputStream = response.getEntity().getContent();
+         HTMLDocumentParser.parse(inputStream);
+      } finally {
+         response.close();
+      }
+   }
 }
